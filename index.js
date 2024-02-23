f5baba5c6a5c94db62530a9f4ea009c120093e4c
tiagolimar <tiagolima.r2@gmail.com>
2024-02-23 18:53:34 -0300

    config cors

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use(cors({
+    origin: "https://pracar2.vercel.app",
+    methods: ['GET', 'POST', 'PATCH'],
+}))
 
 db.connection.sync()
 .then(()=>{

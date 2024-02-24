62223655f446c8d03ad62d2515e3a1764c397c9b
tiagolimar <tiagolima.r2@gmail.com>
2024-02-24 09:16:41 -0300

    update

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin: "*",
-    methods: "GET, POST,PATCH",
+    origin: "https://pracar2.vercel.app/api/praca/cadastrar",
+    methods: "GET, POST, PATCH",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
     optionsSuccessStatus:  204

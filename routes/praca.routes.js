4302015a8e592a3e78d85a0e12fdb5b0533f1001
tiagolimar <tiagolima.r2@gmail.com>
2024-02-24 10:17:26 -0300

    reconfig cors

--- routes/praca.routes.js
+++ routes/praca.routes.js
@@ -1,5 +1,4 @@
-import express from "express"
-import cors from 'cors';
+import express from "express";
 import { pracaController } from './../controller/praca.controller.js';
 import {app} from "../index.js"
 
@@ -14,11 +13,4 @@ export const routePraca = ()=>{
     // router.delete('/pracas', pracaController.deleteALL)
 
     app.use('/api',router)
-    app.use(cors({
-        origin: "https://pracar2.vercel.app",
-        methods: "GET, POST, PATCH",
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true,
-        optionsSuccessStatus:  204
-    }))
 }
